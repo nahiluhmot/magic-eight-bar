f2394006e0c3dc5f5a4d8f4adb07ddd083b7ed34
Tom Hulihan <hulihan.tom159@gmail.com>
2014-07-15 22:03:51 -0400

    Add logging to UsersController#show.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,9 +23,12 @@ class UsersController < ApplicationController
 
   # Get the currently authenticated user.
   def show
+    logger.debug("Looking up user with session: #{params[:id]}")
     if (user = UsersService.get_user(params[:id])).nil?
+      logger.error("No user found for #{params[:id]}")
       render status: 404, body: nil
     else
+      logger.info("Found user for session #{params[:id]}: #{user}")
       render status: 200, json: user.to_json
     end
   end
