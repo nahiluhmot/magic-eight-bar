7b96b8386fcd5602d0b41c60f7fbc8250e8f7b98
Tom Hulihan <hulihan.tom159@gmail.com>
2014-07-16 16:17:59 -0400

    Add logic to read the API key from the environment.

--- gems/places_api/lib/places_api.rb
+++ gems/places_api/lib/places_api.rb
@@ -1,3 +1,19 @@
+require 'excon'
+require 'json'
+require 'hashie'
+
 # This is the top-level module for the gem.
 module PlacesAPI
+  module_function
+
+  # By default, read the api_key from the $GOOGLE_PLACES_API_KEY environment
+  # variable.
+  def api_key
+    @api_key ||= ENV['GOOGLE_PLACES_API_KEY']
+  end
+
+  # Set the api_key programitacally.
+  def api_key=(new_key)
+    @api_key = new_key
+  end
 end
--- gems/places_api/spec/lib/places_api_spec.rb
+++ gems/places_api/spec/lib/places_api_spec.rb
@@ -1,5 +1,33 @@
 require 'spec_helper'
 
 describe PlacesAPI do
-  it { should be_a(Module) }
+  describe '.api_key' do
+    let!(:original) { subject.api_key }
+    let(:test_key) { 'test-key' }
+
+    before { subject.api_key = nil }
+    after { subject.api_key = original }
+
+    context 'when the API key has not been set' do
+      let!(:original_env) {
+        ENV['GOOGLE_PLACES_API_KEY']
+      }
+
+      before { ENV['GOOGLE_PLACES_API_KEY'] = test_key }
+      after { ENV['GOOGLE_PLACES_API_KEY'] = original_env }
+
+      it 'reads from $GOOGLE_PLACES_API_KEY' do
+        expect(subject.api_key).to eq(test_key)
+      end
+    end
+
+    context 'when the API key has been set' do
+      it 'returns that version' do
+        expect { subject.api_key = test_key }
+          .to change { subject.api_key }
+          .from(nil)
+          .to(test_key)
+      end
+    end
+  end
 end
