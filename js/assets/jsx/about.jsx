df8052322a4ecac76d8647c5dc119f88066a8bb5
Tom Hulihan <hulihan.tom159@gmail.com>
2014-08-04 01:05:06 -0400

    Add a BS about page

--- js/assets/jsx/about.jsx
+++ js/assets/jsx/about.jsx
@@ -5,8 +5,18 @@ var Views = Views || {};
 Views.About = React.createClass({
   render: function() {
     return (
-      <div className="container fix-margin">
-        <p>About</p>
+      <div className="container top-level fix-margin">
+        <h1>About</h1>
+
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam vel bibendum lorem. Cras vitae aliquet mauris, ac elementum elit. Proin id ullamcorper est, quis varius purus. Fusce sit amet cursus neque. Praesent eu erat rhoncus, imperdiet quam vitae, vulputate mi. Cras adipiscing risus elit, a venenatis risus faucibus eget. Cras suscipit augue turpis, nec pellentesque massa dignissim et. Aliquam sit amet ultrices sapien. Interdum et malesuada fames ac ante ipsum primis in faucibus. Integer scelerisque diam sit amet elementum feugiat. Maecenas eget aliquam nulla, sed vestibulum mi. Sed lacinia risus lorem. Donec quis laoreet turpis. Vestibulum pharetra facilisis neque.</p>
+
+        <p>Suspendisse ac erat molestie, ornare ante id, consectetur dolor. Pellentesque quis dictum quam. Donec vel consequat massa, non convallis dui. Vestibulum scelerisque pretium ipsum, a fringilla odio imperdiet non. Fusce quis dolor ipsum. In consequat urna non justo accumsan, vitae pretium nisl ullamcorper. Curabitur arcu felis, adipiscing a magna nec, suscipit dignissim justo. Donec tempus turpis vitae lacus suscipit, vel condimentum nisi molestie. Maecenas pretium ligula ut odio lacinia, vel tempus quam blandit. Praesent condimentum varius tellus ullamcorper imperdiet. Nunc porttitor lectus sapien, at eleifend dui consequat et. Interdum et malesuada fames ac ante ipsum primis in faucibus. Vestibulum eu ultrices tellus. Vivamus non sapien ut erat sollicitudin tristique. Donec gravida euismod neque ut vehicula. Morbi dictum accumsan molestie.</p>
+
+        <p>Sed ut urna venenatis, bibendum massa tincidunt, vulputate ligula. Sed elementum nisl orci, vel scelerisque neque gravida nec. Nullam laoreet suscipit convallis. Phasellus mi dui, molestie et porta ac, hendrerit sed orci. Praesent id nunc eros. Ut auctor, est varius tempus fermentum, mauris arcu pretium libero, sit amet congue ante quam sed ligula. Etiam rhoncus augue sit amet tortor porttitor, quis sollicitudin metus interdum.</p>
+
+        <p>Nunc vel orci pharetra, iaculis orci ut, consequat tellus. Vestibulum rutrum augue nec libero iaculis, ut ullamcorper diam viverra. Maecenas in mattis urna. Integer sit amet ultrices nisi, vitae sagittis risus. Curabitur id neque vitae metus euismod fermentum non nec nibh. Ut eu mi nec nisi consequat viverra. Vestibulum ac congue nulla, sit amet gravida ipsum. Suspendisse a malesuada nunc. Fusce rhoncus luctus nunc. Maecenas pharetra tristique mauris, at vulputate eros porttitor quis. Donec a varius mi, non lobortis ipsum. Morbi leo risus, dictum nec nulla commodo, congue vestibulum libero.</p>
+
+        <p>Sed nec blandit nisl, eget sollicitudin purus. Phasellus at quam faucibus, faucibus nunc quis, fringilla velit. Donec eget eros posuere, tincidunt nulla ut, lacinia purus. Maecenas tincidunt lectus a velit blandit, at lobortis sem consectetur. Donec arcu sapien, eleifend sed vestibulum eget, aliquet in magna. Aliquam luctus tellus ac molestie egestas. Maecenas accumsan lorem at lorem viverra, quis blandit nisi placerat. Nam sollicitudin pellentesque rutrum. Curabitur semper dui eu dui volutpat vestibulum. Suspendisse in orci ac velit vulputate sodales ut sed eros. Donec vulputate dui orci, nec commodo purus luctus sed.</p>
       </div>
     );
   }
