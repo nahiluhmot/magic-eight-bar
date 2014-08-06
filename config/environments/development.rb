93e85765ef07ac5098003fdb2ae07ca10959dea8
Tom Hulihan <hulihan.tom159@gmail.com>
2014-08-06 09:15:43 -0400

    Log to STDOUT

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -22,6 +22,8 @@ Rails.application.configure do
   # Raise an error on page load if there are pending migrations.
   config.active_record.migration_error = :page_load
 
+  # Log to STDOUT
+  config.logger = ActiveSupport::TaggedLogging.new(Logger.new(STDOUT))
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
